4af5baa1fff6b0aa78f5e54d55efaa002c4ad448
Shivansh Soni <shivanshsoni23@gmail.com>
2018-10-15 13:48:09 +0530

    Create codes25.java

---
+++ codes25.java
@@ -0,0 +1,16 @@
+import java.util.Scanner;
+class code25
+{
+  public static void main(String[]args)
+  {
+    Scanner scan=new Scanner(System.in);
+    int t,i,j,k,l;
+    t=scan.nextInt();
+    for(i=0;i<t;i++)
+    {
+      int a=scan.nextInt();
+      int b=scan.nextInt();
+      System.out.println(a+b);
+    }
+  }
+}
